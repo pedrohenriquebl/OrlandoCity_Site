921e7f1db83cea2b96798d04306c2ed745cab3dc
Pedro Henrique <pedruhbl@yahoo.com.br>
2021-09-02 10:22:46 -0300

    create a jQuery to apply play on a video through the click on images at video.html

---
+++ js/efeitos.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    
+    $("#logotipo").on("mouseover", function(){
+
+        $("#banner h1").addClass("efeito");
+
+    }).on("mouseout", function(){
+
+        $("#banner h1").removeClass("efeito");
+    });
+
+    $("#input-search").on("focus", function(){
+
+        $("li.search").addClass("ativo");            
+    
+    }).on("blur", function(){
+        $("li.search").removeClass("ativo");
+    })     
+    
+    $(".thumbnails").owlCarousel({
+        stagePadding: 20,
+        loop:true,
+        margin:20,
+        nav:true,
+        navText: ["Anterior", "Próximo"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
